cdbdc13f7e854218e18434dc2e59aaa0452142ea
Alexandre David <alexandre.David42@gmail.com>
2020-09-19 22:25:04 +0200

    Ration edit

--- src/components/Ration.vue
+++ src/components/Ration.vue
@@ -2,11 +2,19 @@
   <ion-content>
     <ion-list>
       <ion-item>
-        <ion-label>Titre</ion-label>
-        <ion-input />
+        <ion-label position="floating">Type</ion-label>
+        <ion-select @ionChange="type = $event.target.value" :value="type">
+          <ion-select-option value="ration">Fiche ration</ion-select-option>
+          <ion-select-option value="alim">Préférences alimentaire</ion-select-option>
+          <ion-select-option value="training">Training</ion-select-option>
+          <ion-select-option value="behaviour">Comportement</ion-select-option>
+          <ion-select-option value="enrichment">Enrichissements</ion-select-option>
+          <ion-select-option value="treatment">Traitement en cours</ion-select-option>
+          <ion-select-option value="observe">Observations</ion-select-option>
+        </ion-select>
       </ion-item>
       <ion-item>
-        <ion-label>Animaux</ion-label>
+        <ion-label position="floating">Animaux</ion-label>
         <ion-select multiple>
           <ion-select-option value="macaques">Macaques</ion-select-option>
           <ion-select-option value="kiki">Kiki</ion-select-option>
@@ -19,33 +27,46 @@
           <ion-select-option value="observation">Descartes</ion-select-option>
         </ion-select>
       </ion-item>
+      <ion-item v-if="type === 'ration'">
+        <ion-label position="floating">Titre</ion-label>
+        <ion-input />
+      </ion-item>
+      <ion-item v-else>
+         <ion-label position="floating">Description</ion-label>
+        <ion-textarea />
+      </ion-item>
     </ion-list>
-    <ion-card v-for="(ration, key) in rations" :key="key">
-      <ion-card-title>
-        <ion-item>
-          <ion-label position="floating">Nom de la ration</ion-label>
-          <ion-input @ionInput="ration.name = $event.target.value" :value="ration.name" />
-        </ion-item>
-      </ion-card-title>
-      <ion-card-content>
-        <ion-item>
-          <ion-label position="floating">Contenu</ion-label>
-          <ion-textarea @ionInput="ration.content = $event.target.value" auto-grow></ion-textarea>
-        </ion-item>
-      </ion-card-content>
-    </ion-card>
-    <ion-button expand="block" margin-bottom @click="addRation">
-      <ion-icon name="add"></ion-icon>
-      Ajouter une autre ration
-    </ion-button>
+    <template v-if="type === 'ration'">
+      <ion-card v-for="(ration, key) in rations" :key="key">
+        <ion-card-title>
+          <ion-item>
+            <ion-label position="floating">Nom de la ration</ion-label>
+            <ion-input @ionInput="ration.name = $event.target.value" :value="ration.name" />
+          </ion-item>
+        </ion-card-title>
+        <ion-card-content>
+          <ion-item>
+            <ion-label position="floating">Contenu</ion-label>
+            <ion-textarea @ionInput="ration.content = $event.target.value" auto-grow></ion-textarea>
+          </ion-item>
+        </ion-card-content>
+      </ion-card>
+      <ion-button expand="block" margin-bottom @click="addRation">
+        <ion-icon name="add"></ion-icon>
+        Ajouter une autre ration
+      </ion-button>
+    </template>
   </ion-content>
 </template>
 
 <script>
+import { db } from '@/plugins/firebase'
+
 export default {
   name: 'Ration',
   data () {
     return {
+      type: 'ration',
       title: '',
       rations: [
         { name: '', content: '' }
@@ -55,6 +76,9 @@ export default {
   methods: {
     addRation () {
       this.rations.push({ name: '', content: '' })
+    },
+    async save (form) {
+      await db.collection('infos').add(form)
     }
   }
 }
--- src/views/animal/details/HealtherDoc.vue
+++ src/views/animal/details/HealtherDoc.vue
@@ -3,7 +3,37 @@
     <ion-list>
       <ion-item>
         <ion-label>
-          <h3>10 aout 2019</h3>
+          <h3>Fiche ration</h3>
+          <p>L'individu a été anesthésié pour déplacement suite à changement d'enclos</p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>
+          <h3>Préférence alimentaire</h3>
+          <p>L'individu a été anesthésié pour déplacement suite à changement d'enclos</p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>
+          <h3>Training</h3>
+          <p>L'individu a été anesthésié pour déplacement suite à changement d'enclos</p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>
+          <h3>Enrichissements</h3>
+          <p>L'individu a été anesthésié pour déplacement suite à changement d'enclos</p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>
+          <h3>Traitement en cours</h3>
+          <p>L'individu a été anesthésié pour déplacement suite à changement d'enclos</p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>
+          <h3>Observations</h3>
           <p>L'individu a été anesthésié pour déplacement suite à changement d'enclos</p>
         </ion-label>
       </ion-item>
